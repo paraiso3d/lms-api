e31573a1c415d0bb6503a7ae78230f8a24330038
paraiso3d <normanparaiso.abm12@gmail.com>
2025-12-05 23:16:04 +0800

    AA

--- app/Models/DiscussionLike.php
+++ app/Models/DiscussionLike.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DiscussionLike extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'discussion_id',
+        'user_id',
+    ];
+
+    public function discussion()
+    {
+        return $this->belongsTo(Discussion::class, 'discussion_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
